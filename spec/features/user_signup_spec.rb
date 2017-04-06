7df8e2063e3234d29e031ecd7bb8abb3209289e6
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-04-06 12:08:08 +0100

    Add feature test to add new user

---
+++ spec/features/user_signup_spec.rb
@@ -0,0 +1,10 @@
+feature 'User signup' do
+  scenario "Add user" do
+    visit('/users/new')
+    fill_in "email", with: "test@test.com"
+    fill_in "password", with: "password"
+    click_button "Create Account"
+
+    expect(page).to have_xpath("//links")
+  end
+end
